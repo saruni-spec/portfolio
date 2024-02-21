4fe549661c32c3011326d6921742a851c353429a
boss <smithsaruni16@gmail.com>
2024-02-21 21:39:02 +0300

    all

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,18 @@
+import * as images from "../images";
+
+
 const Projects = () => {
-  return <div>Projects</div>;
+  const imageKeys = Object.keys(images);
+
+  return (
+    <div>
+      {imageKeys.map((key, i) => (
+        <div key={i} className="imageDiv">
+          <img src={images[key]} alt={`Project ${i}`} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
